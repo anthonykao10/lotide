64a9239c76673e03b3aea2a18c9058ac5c23a0ba
anthonykao10 <anthonykao10@gmail.com>
2019-08-23 20:47:32 +0000

    Implement eqObjectsRecursive

---
+++ eqObjectsRecursive.js
@@ -0,0 +1,44 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
+};
+
+// Returns true if both objects have identical keys with identical values.
+// Otherwise return false.
+const eqObjects = function(obj1, obj2) {
+  if (Object.keys(obj1).length !== Object.keys(obj2).length) return false;
+
+  for (let key in obj1) {
+    // IF val is an array, check for equality with 'eqArrays'
+    if (Array.isArray(obj1[key]) && Array.isArray(obj2[key])) {
+      return eqArrays(obj1[key], obj2[key]);
+    }
+
+    // IF val is object AND NOT array:
+    if (typeof obj1[key] === 'object' && typeof obj2[key] === 'object') {
+      return eqObjects(obj1[key], obj2[key]);
+    }
+    // Otherwise, check val as primitives
+    if (obj1[key] !== obj2[key]) return false;
+  }
+  return true;
+};
+
+
+
+// TEST CASES:
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`\u274C Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`\u2714 Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true);
+assertEqual(eqObjects({ a: { z: 1, x: {y: 5} }, b: 2 }, { a: { z: 1, x: {y: 5} }, b: 2 }), true);
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false);
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false);
