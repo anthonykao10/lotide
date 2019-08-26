5745a02906cf5f2040cf71b9b58b51b99225144d
anthonykao10 <anthonykao10@gmail.com>
2019-08-26 23:20:06 +0000

    Remove unnecessary comments from eqObjects

--- eqObjects.js
+++ eqObjects.js
@@ -22,20 +22,3 @@ const eqObjects = function(obj1, obj2) {
 };
 
 module.exports = eqObjects;
-
-// Check primitive values:
-// const ab = { a: "1", b: "2" };
-// const ba = { b: "2", a: "1" };
-// assertEqual(eqObjects(ab, ba), true);
-
-// const abc = { a: "1", b: "2", c: "3" };
-// assertEqual(eqObjects(ab, abc), false);
-
-// // Check array values:
-// const cd = { c: "1", d: ["2", 3] };
-// const dc = { d: ["2", 3], c: "1" };
-// assertEqual(eqObjects(cd, dc), true);
-
-// const cd2 = { c: "1", d: ["2", 3, 4] };
-// assertEqual(eqObjects(cd, cd2), false);
-// assertEqual(eqObjects(cd2, cd2), true);
