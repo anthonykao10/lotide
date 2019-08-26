50b4008f188aaecfe32d285e154615e4c31c736c
anthonykao10 <anthonykao10@gmail.com>
2019-08-26 23:40:41 +0000

    Refactor findKey tests with M & C

--- findKey.js
+++ findKey.js
@@ -8,34 +8,4 @@ const findKey = function(obj, cb) {
   return undefined;
 };
 
-
-// TESTS
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    console.log(`\u274C Assertion Failed: ${actual} !== ${expected}`);
-  } else {
-    console.log(`\u2714 Assertion Passed: ${actual} === ${expected}`);
-  }
-};
-
-// console.log(findKey());
-
-let test = findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2); // => "noma"
-
-assertEqual(test, 'noma');
-
-assertEqual(findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars > 2), 'Akaleri');
+module.exports = findKey;
---
+++ test/findKeyTest.js
@@ -0,0 +1,23 @@
+const assert = require('chai').assert;
+const findKey = require('../findKey');
+
+describe('#findKey', () => {
+
+  it('should return the first matching key, given a certain condition (from callback)', () => {
+    let test = findKey({
+      "Blue Hill": { stars: 1 },
+      "Akaleri":   { stars: 3 },
+      "noma":      { stars: 2 },
+      "elBulli":   { stars: 3 },
+      "Ora":       { stars: 2 },
+      "Akelarre":  { stars: 3 }
+    }, x => x.stars === 2);
+    assert.strictEqual(test, 'noma');
+  });
+
+  it('should return undefined, given an empty object', () => {
+    let test = findKey({}, x => x.stars === 2);
+    assert.strictEqual(test, undefined);
+  });
+
+});
