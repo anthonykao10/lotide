1f9563af15ace6be029bcde461c4ad38001fdf47
anthonykao10 <anthonykao10@gmail.com>
2019-08-27 00:27:29 +0000

    Cleanup tests

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,12 +1,12 @@
 const assertEqual = require('../assertEqual');
 
 // identical strings:
-assertEqual("Hippo", "Hippo");
+// assertEqual("Hippo", "Hippo");
 // non-identical strings:
-assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual("Lighthouse Labs", "Bootcamp");
 // identical numbers
-assertEqual(1, 1);
+// assertEqual(1, 1);
 // non-identical numbers
-assertEqual(1, -1);
+// assertEqual(1, -1);
 // different types
-assertEqual(3, '3');
+// assertEqual(3, '3');
